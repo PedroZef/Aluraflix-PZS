6866324efba58fabb8acf0f57721990a59101416
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2023-09-12 19:14:28 -0300

    Contexts criado

--- src/data/db.js
+++ src/data/db.js
@@ -14,11 +14,12 @@ let videos = [
   {
     id: uuidv4(),
     categoryName: "frontend",
-    categoryDisplayName: "Frontend",
-    title: "O que todo iniciante em Front End deveria aprender",
-    description: "Como manipular arrays e objetos em JavaScript",
-    videoImgLink: "https://img.youtube.com/vi/yS7AcF-xRUg/hqdefault.jpg",
-    videoLink: "https://www.youtube.com/watch?v=yS7AcF-xRUg",
+    categoryDisplayName: "Front End",
+    title: "O que é React JS",
+    description:
+      "Afinal, o que é React JS? Neste vídeo, Vanessa Tonini e Mario Souto explicam isto para você falando sobre como esta ferramenta surgiu, para que ela serve, quais são suas aplicações e relação com outras ferramentas e como começar a trabalhar com ele. ",
+    videoImgLink: "https://i.ytimg.com/vi/6IuQUgeDPg0/hq720.jpg",
+    videoLink: "https://www.youtube.com/watch?v=6IuQUgeDPg0",
   },
 
   {
@@ -31,16 +32,7 @@ let videos = [
     videoImgLink: "https://img.youtube.com/vi/jK0uiQ1ZQQQ/maxresdefault.jpg",
     videoLink: "https://www.youtube.com/watch?v=jK0uiQ1ZQQQ",
   },
-  {
-    id: uuidv4(),
-    categoryName: "datascience",
-    categoryDisplayName: "Data Science",
-    title: "O que é React JS",
-    description:
-      "Afinal, o que é React JS? Neste vídeo, Vanessa Tonini e Mario Souto explicam isto para você falando sobre como esta ferramenta surgiu, para que ela serve, quais são suas aplicações e relação com outras ferramentas e como começar a trabalhar com ele. ",
-    videoImgLink: "https://i.ytimg.com/vi/6IuQUgeDPg0/hq720.jpg",
-    videoLink: "https://www.youtube.com/watch?v=6IuQUgeDPg0",
-  },
+
   {
     id: uuidv4(),
     categoryName: "datascience",
@@ -49,6 +41,17 @@ let videos = [
     description: "O que é Machine Learning? #HipstersPontoTube",
     videoImgLink: "https://img.youtube.com/vi/Iuz_jc96bQk/hqdefault.jpg",
     videoLink: "https://www.youtube.com/watch?v=Iuz_jc96bQk",
+  },
+  {
+    id: uuidv4(),
+    categoryName: "datascience",
+    categoryDisplayName: "Data Science",
+    title: "Big Data com Apache Spark | #AluraMais",
+    description:
+      "O que é Apache Spark? Conheça essa tecnologia de Big Data a partir da evolução Hadoop vs. Spark e entenda como funciona sua lógica interna.",
+
+    videoImgLink: "https://img.youtube.com/vi/3aSfKsBhU5E/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/watch?v=3aSfKsBhU5E",
   },
   {
     id: uuidv4(),
@@ -70,7 +73,7 @@ let videos = [
     videoImgLink: "https://img.youtube.com/vi/p4fiAiiPi3E/maxresdefault.jpg",
     videoLink: "https://www.youtube.com/watch?v=p4fiAiiPi3E",
   },
-   {
+  {
     id: uuidv4(),
     categoryName: "mobile",
     categoryDisplayName: "Mobile",
@@ -89,7 +92,6 @@ let videos = [
     videoImgLink: "https://img.youtube.com/vi/ldRbnpy-qQs/hqdefault.jpg",
     videoLink: "https://www.youtube.com/watch?v=ldRbnpy-qQs",
   },
- 
 ];
 
 let categories = [
